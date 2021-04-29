545ad47d88ef8ac61c8ab674fca256610aa3c1d5
acailly <ylliac14@gmail.com>
2021-04-29 21:19:32 +0200

    :sparkles: Fuck FLoC

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 app.use((req, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  // See https://cleanuptheweb.org/
+  res.header("Permissions-Policy", "interest-cohort=()");
   next();
 });
 
